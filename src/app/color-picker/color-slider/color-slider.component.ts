9cb3a668785f528cddd81f424b67ad3d282a6993
Ebruesra <ebruesra06@gmail.com>
2019-12-26 10:39:06 +0300

    upload

---
+++ src/app/color-picker/color-slider/color-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color-slider',
+  templateUrl: './color-slider.component.html',
+  styleUrls: ['./color-slider.component.css']
+})
+export class ColorSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
